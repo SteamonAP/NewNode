f55ef5928efbec8bc790a68cbb0ea3e32551e3bc
SteamonAP <amoghpitale7@gmail.com>
2025-03-06 21:50:49 +0530

    basics of express and web

---
+++ module2/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const path = require('path');
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`The server's running on ${PORT}`);
+});
