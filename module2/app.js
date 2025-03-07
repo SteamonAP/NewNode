55093dd6efb5302d5be46396cc3585a341e944f3
SteamonAP <amoghpitale7@gmail.com>
2025-03-07 22:07:52 +0530

    added ejs

--- module2/app.js
+++ module2/app.js
@@ -3,22 +3,25 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 const path = require('path');
+const app = express();
 
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404', {pageTitle: "Page not Found"});
 })
 
 app.listen(PORT, () => {
--- module2/routes/shop.js
+++ module2/routes/shop.js
@@ -4,11 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    const products = adminData.product;
+    res.render('shop',{prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length>0,activeShop: true, productCSS: true})
 });
 
 module.exports = router;
