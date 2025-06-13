260085b8e50176d469070e67500e2689bc66caa6
SteamonAP <amoghpitale7@gmail.com>
2025-06-13 13:37:58 +0530

    added onbrowser/async deletion

--- module2/controllers/admin.js
+++ module2/controllers/admin.js
@@ -184,8 +184,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -195,11 +195,10 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({_id: prodId, userId: req.user._id});
     })
     .then(() => {
-      res.redirect("/admin/products");
+      res.status(200).json({message: "Deleted successfully!"});
     })
     .catch((err) => {
-      const error = new Error('Creating prodect failed',err)
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message: 'Deletion failed', error: err});
+      
     });
 };
---
+++ module2/public/js/admin.js
@@ -0,0 +1,25 @@
+const deleteProduct = (btn) =>{
+    // console.log('Clicked');
+    const prodId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+    const product = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+    .then(result => {
+        return result.json()
+    })
+    .then(data => {
+        console.log(data.message);
+        alert(data.message);
+        product.parentNode.removeChild(product);
+
+    })
+    .catch(err =>{
+        console.log(err);
+    });
+};
